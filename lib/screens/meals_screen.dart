62852b6e18a1cd7ff1dc9738bf8dccd9f7cefdc2
Fenysk <minefenysk@gmail.com>
2024-05-21 10:28:10 +0200

    🚧 Update MealsScreen to display meals and handle empty state

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -13,10 +14,38 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = Column(
+      children: [
+        Text(
+          'Oups !',
+          style: Theme.of(context)
+              .textTheme
+              .headlineLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          'Aucun repas trouvé, vérifiez vos filtres !',
+          style: Theme.of(context)
+              .textTheme
+              .headlineLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ],
+    );
+
+    if (meals.isNotEmpty) {
+      mainContent = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (context, index) => MealItem(meal: meals[index]),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
+      body: mainContent,
     );
   }
 }
---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(meal.title),
+    );
+  }
+}
