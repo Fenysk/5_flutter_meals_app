197cea15d55e1f74bf5f90411108d8aa522f86df
Fenysk <minefenysk@gmail.com>
2024-05-21 14:12:34 +0200

    🚧 MealDetailsScreen with image, ingredients, and steps

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -9,11 +9,89 @@ class MealDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(meal.title),
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 200,
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Ingrédients',
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                    textAlign: TextAlign.start,
+                  ),
+                  const SizedBox(height: 14),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      for (var ingredient in meal.ingredients)
+                        Text(
+                          '⸱ $ingredient',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        )
+                    ],
+                  ),
+                  const SizedBox(height: 32),
+                  Text(
+                    'Étapes',
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                    textAlign: TextAlign.start,
+                  ),
+                  const SizedBox(height: 14),
+                  Column(
+                    children: [
+                      for (var i = 0; i < meal.steps.length; i++)
+                        Column(
+                          children: [
+                            Text(
+                              '${i + 1}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              meal.steps[i],
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 24),
+                          ],
+                        )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
-        body: const Center(
-          child: Text('Meal Details'),
-        ));
+      ),
+    );
   }
 }
