c2f288c88288d87c3dd0a40e8069ad2ec16f20da
Fenysk <minefenysk@gmail.com>
2024-05-21 09:41:31 +0200

    🚧 Add more dummy data

--- lib/data/dummy_categories.dart
+++ lib/data/dummy_categories.dart
@@ -6,4 +6,10 @@ final dummyCategories = [
   Category(title: 'Repas copieux', color: Colors.orange),
   Category(title: 'Végétarien', color: Colors.green),
   Category(title: 'Petit-déjeuner', color: Colors.lightBlue),
+  Category(title: 'Asiatique', color: Colors.purple),
+  Category(title: 'Français', color: Colors.red),
+  Category(title: 'Été', color: Colors.blue),
+  Category(title: 'Hiver', color: Colors.indigo),
+  Category(title: 'Exotique', color: Colors.teal),
+  Category(title: 'Santé', color: Colors.pink),
 ];
