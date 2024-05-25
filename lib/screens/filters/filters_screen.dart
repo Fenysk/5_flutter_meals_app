a728a15d9c73bb9ac25ab12d5a50a76b09ef5642
Fenysk <minefenysk@gmail.com>
2024-05-25 11:47:49 +0200

    🚧 Return filter values with PopScope

--- lib/screens/filters/filters_screen.dart
+++ lib/screens/filters/filters_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-// import 'package:meals_app/widgets/main_drawer.dart';
-// import 'package:meals_app/screens/tabs_screen.dart';
+
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegan,
+  vegetarian,
+}
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -21,117 +26,101 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Filtres'),
       ),
-      // drawer: MainDrawer(
-      //   onSelectScreen: (indentifier) {
-      //     Navigator.of(context).pop();
-      //     if (indentifier == 'meals') {
-      //       Navigator.of(context).pushReplacement(
-      //         MaterialPageRoute(
-      //           builder: (context) => const TabsScreen(),
-      //         ),
-      //       );
-      //     }
-      //   },
-      // ),
-      body: Column(
-        children: [
-          SwitchListTile(
-            value: _isGlutenFreeSet,
-            onChanged: (bool newValue) {
-              setState(() {
-                _isGlutenFreeSet = newValue;
-              });
-            },
-            title: Text(
-              'Sans gluten',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+      body: PopScope(
+        canPop: false,
+        onPopInvoked: ((didPop) {
+          if (didPop) return;
+          Navigator.of(context).pop({
+            Filter.glutenFree: _isGlutenFreeSet,
+            Filter.lactoseFree: _isLactoseFreeSet,
+            Filter.vegan: _isVeganSet,
+            Filter.vegetarian: _isVegetarianSet,
+          });
+        }),
+        child: Column(
+          children: [
+            SwitchListTile(
+              value: _isGlutenFreeSet,
+              onChanged: (bool newValue) {
+                setState(() {
+                  _isGlutenFreeSet = newValue;
+                });
+              },
+              title: Text(
+                'Sans gluten',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: Text(
+                'Exclut tous les aliments contenant du gluten.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
             ),
-            subtitle: Text(
-              'Exclut tous les aliments contenant du gluten.',
-              style: Theme.of(context)
-                  .textTheme
-                  .labelMedium!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            SwitchListTile(
+              value: _isLactoseFreeSet,
+              onChanged: (bool newValue) {
+                setState(() {
+                  _isLactoseFreeSet = newValue;
+                });
+              },
+              title: Text(
+                'Sans lactose',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: Text(
+                'Convient aux intolérants au lactose.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
             ),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-            contentPadding: const EdgeInsets.only(left: 34, right: 22),
-          ),
-          SwitchListTile(
-            value: _isLactoseFreeSet,
-            onChanged: (bool newValue) {
-              setState(() {
-                _isLactoseFreeSet = newValue;
-              });
-            },
-            title: Text(
-              'Sans lactose',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            SwitchListTile(
+              value: _isVeganSet,
+              onChanged: (bool newValue) {
+                setState(() {
+                  _isVeganSet = newValue;
+                });
+              },
+              title: Text(
+                'Végan',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: Text(
+                'Aucun produit d’origine animale inclus.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
             ),
-            subtitle: Text(
-              'Convient aux intolérants au lactose.',
-              style: Theme.of(context)
-                  .textTheme
-                  .labelMedium!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            SwitchListTile(
+              value: _isVegetarianSet,
+              onChanged: (bool newValue) {
+                setState(() {
+                  _isVegetarianSet = newValue;
+                });
+              },
+              title: Text(
+                'Végétarien',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: Text(
+                'Aucune viande ni poisson inclus.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
             ),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-            contentPadding: const EdgeInsets.only(left: 34, right: 22),
-          ),
-          SwitchListTile(
-            value: _isVeganSet,
-            onChanged: (bool newValue) {
-              setState(() {
-                _isVeganSet = newValue;
-              });
-            },
-            title: Text(
-              'Végan',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            subtitle: Text(
-              'Aucun produit d’origine animale inclus.',
-              style: Theme.of(context)
-                  .textTheme
-                  .labelMedium!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-            contentPadding: const EdgeInsets.only(left: 34, right: 22),
-          ),
-          SwitchListTile(
-            value: _isVegetarianSet,
-            onChanged: (bool newValue) {
-              setState(() {
-                _isVegetarianSet = newValue;
-              });
-            },
-            title: Text(
-              'Végétarien',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            subtitle: Text(
-              'Aucune viande ni poisson inclus.',
-              style: Theme.of(context)
-                  .textTheme
-                  .labelMedium!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-            contentPadding: const EdgeInsets.only(left: 34, right: 22),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
